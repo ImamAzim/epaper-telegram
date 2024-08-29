7f11a6be858a98b7161cede1239ab9607cd73343
Imam Usmani <imam@e.email>
2024-08-29 21:05:39 +0200

    main app

--- src/epaper-telegram/apps.py
+++ src/epaper-telegram/apps.py
@@ -1,11 +1,28 @@
-#!/usr/bin/env python
+import logging
 
 
-"""
-this is my module
-"""
+class epaper_telegram_app(object):
 
-pass
+    """app to launch the main app of the project"""
+
+    def __init__(self):
+        pass
+
+    def start(self):
+        """method to start the app.
+        :returns: TODO
+
+        """
+        print('start')
+
+
+def launch_epaper_telegram():
+    """lauch the main app. you can add verbose mode to debug
+    :returns: TODO
+
+    """
+    app = epaper_telegram_app()
+    app.start()
 
 
 if __name__ == '__main__':
