087bf33eeb592f6aaa76b74ece432ee03511e308
imam raspix2 <imam@e.email>
2024-08-31 23:06:20 +0200

    init gt1115

--- src/epaper_telegram/apps.py
+++ src/epaper_telegram/apps.py
@@ -1,4 +1,5 @@
 import logging
+import threading
 
 
 from gpiozero.pins.native import NativeFactory
@@ -12,7 +13,17 @@ class EpaperTelgramApp(object):
     """app to launch the main app of the project"""
 
     def __init__(self):
-        pass
+        self._flag_t = 1
+
+        self._gt = gt1151.GT1151()
+        self._GT_Dev = gt1151.GT_Development()
+        self._GT_Old = gt1151.GT_Development()
+
+        logging.info("init touch screen")
+
+        self._gt.GT_Init()
+        self._thread_gt = threading.Thread(target = self._pthread_irq)
+        self._thread_gt.setDaemon(True)
 
     def start(self):
         """method to start the app.
@@ -21,6 +32,16 @@ class EpaperTelgramApp(object):
         """
         logging.info('start')
         logging.debug('debug mode')
+
+
+    def _pthread_irq() :
+        print("pthread running")
+        while self._flag_t == 1 :
+            if(gt.digital_read(gt.INT) == 0) :
+                GT_Dev.Touch = 1
+            else :
+                GT_Dev.Touch = 0
+        print("thread:exit")
 
 
 if __name__ == '__main__':
