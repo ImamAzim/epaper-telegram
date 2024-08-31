cc9c0df197f0e136717f5f2a95831fbfb061f420
Imam Usmani <imam@e.email>
2024-08-31 22:29:49 +0200

    log config in kwargs

--- src/epaper_telegram/launchers.py
+++ src/epaper_telegram/launchers.py
@@ -18,26 +18,20 @@ def launch_epaper_telegram():
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('-l', '--logfile', action='store_true')
     args = parser.parse_args()
+
     if args.verbose:
         level = logging.DEBUG
     else:
         level = logging.INFO
+    logging_config_kwargs = dict(
+            encoding='utf-8',
+            format="%(asctime)s %(name)s.%(levelname)s: %(message)s",
+            datefmt="%Y.%m.%d %H:%M:%S",
+            level=level,
+            )
     if args.logfile:
-        filename = 'epaper-telgram.log'
-        logging.basicConfig(
-                filename=filename,
-                encoding='utf-8',
-                format="%(asctime)s %(name)s.%(levelname)s: %(message)s",
-                datefmt="%Y.%m.%d %H:%M:%S",
-                level=level,
-                )
-    else:
-        logging.basicConfig(
-                encoding='utf-8',
-                format="%(asctime)s %(name)s.%(levelname)s: %(message)s",
-                datefmt="%Y.%m.%d %H:%M:%S",
-                level=level,
-                )
+        logging_config_kwargs['filename'] = 'epaper-telegram.log'
+    logging.basicConfig(**logging_config_kwargs)
 
     app = EpaperTelgramApp()
     app.start()
