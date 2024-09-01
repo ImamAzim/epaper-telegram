425babd57cf4d5d49292a1d5bfa3392cddc8cf3c
Imam Usmani <imam@e.email>
2024-09-01 10:24:19 +0200

    create class

---
+++ src/epaper_telegram/models/waveshare_touch_epaper.py
@@ -0,0 +1,7 @@
+class EpdTouch2In13(object):
+
+    """handle waveshare touch epaper display 2inch13. both display and touch device"""
+
+    def __init__(self):
+        """TODO: to be defined. """
+        pass
