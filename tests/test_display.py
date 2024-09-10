95c1f255546e148ece0cdb0d3492b564fc69ce76
Imam Usmani <imam@e.email>
2024-09-10 21:05:16 +0200

    test display img

--- tests/test_display.py
+++ tests/test_display.py
@@ -2,8 +2,10 @@
 test display module
 """
 
-import os
 import unittest
+
+
+from PIL import Image, ImageDraw
 
 
 from epaper_telegram.models.display import Displayer
@@ -19,10 +21,16 @@ class TestMyClass(unittest.TestCase):
 
     pass
 
+
 """ script tests """
+
 
 def display_img():
     displayer = Displayer()
+    img = Image.new('1', (250, 122), 255)
+    draw = ImageDraw.Draw(img)
+    draw.text((8, 12), 'hello world', fill=255)
+    displayer.display_img(img)
 
 
 if __name__ == '__main__':
