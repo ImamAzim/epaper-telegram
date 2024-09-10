4a1821bba2dba08ef4b50f41fc1db31d9c1f74d2
Imam Usmani <imam@e.email>
2024-09-10 21:08:34 +0200

    add sleep after param

--- src/epaper_telegram/models/display.py
+++ src/epaper_telegram/models/display.py
@@ -6,11 +6,12 @@ class Displayer(object):
     def __init__(self):
         pass
 
-    def display_img(self, img, block=True, timeout=None):
+    def display_img(self, img, sleep_after=True, block=True, timeout=None):
         """the img will we put in a queue and display once the worker is ready.
         to avoid being blocked, one can use the wait_for_ready method
 
         :img: array of dim (250, 122) to be displayed
+        :sleep_after: bool, put the display to sleep after task done
         :block: bool, same function as in a queue object
         :timeout: bool or positive number, same function as in a queue object
 
