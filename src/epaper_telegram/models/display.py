c1ab727ca32d17f2f0bf6b76b429b1c73cc24bdc
Imam Usmani <imam@e.email>
2024-09-10 20:54:14 +0200

    def display img

--- src/epaper_telegram/models/display.py
+++ src/epaper_telegram/models/display.py
@@ -5,3 +5,14 @@ class Displayer(object):
 
     def __init__(self):
         pass
+
+    def display_img(self, img, block=True, timeout=None):
+        """the img will we put in a queue and display once the worker is ready.
+        to avoid being blocked, one can use the wait_for_ready method
+
+        :img: array of dim (250, 122) to be displayed
+        :block: bool, same function as in a queue object
+        :timeout: bool or positive number, same function as in a queue object
+
+        """
+        pass
