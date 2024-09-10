e19ab52c2c5e6676b2ab85e9712cfe3a0afbf6a6
Imam Usmani <imam@e.email>
2024-09-10 20:44:22 +0200

    class displayer

---
+++ src/epaper_telegram/models/display.py
@@ -0,0 +1,7 @@
+class Displayer(object):
+
+    """object that will take img array and display on epaper (epd waveshare).
+    Should be the only one to use this resource"""
+
+    def __init__(self):
+        pass
