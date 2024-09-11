e84d5a9cd631591b403014bb9e795c839ce9166f
Imam Usmani <imam@e.email>
2024-09-11 23:07:18 +0200

    linter

--- src/epaper_telegram/models/img_creators.py
+++ src/epaper_telegram/models/img_creators.py
@@ -8,7 +8,8 @@ from PIL import Image
 class DrawTool(object):
 
     """this class will take coordinates, draw them and send it to a displayer.
-    it includes a menu for the user, so that it can ask for more coordinates or not"""
+    it includes a menu for the user, so that it can ask for more coordinates
+    or not"""
 
     def __init__(self, displayer):
         """TODO: to be defined.
@@ -17,19 +18,23 @@ class DrawTool(object):
         self._displayer = displayer
         self._img = None
 
-        self._queue = queue.Queue()
-        self._thread = Thread(target=self._process_coordinates_loop, daemon=True)
+        self._queue = Queue()
+        self._thread = Thread(
+                target=self._process_coordinates_loop,
+                daemon=True,
+                )
         self._thread.start()
 
         self._reset_img()
 
-
     def point_to(self, x, y):
         """
-        points the pen to coordinate x, y. If it is in the drawing area, it will be drawn on the image.
+        points the pen to coordinate x, y. If it is in the drawing area, it
+        will be drawn on the image.
         When the displayer is ready, will update and the img with all
         the points will be sent to the displayer.
-        if the point is on a menu, it can clear the image, or ask to stop (and ev return the img)
+        if the point is on a menu, it can clear the image, or ask to stop
+        (and ev return the img)
 
         :x: x coordinates to point on the display
         :y: y coordinates to point on the display
@@ -40,13 +45,14 @@ class DrawTool(object):
 
     def clear_img(self):
         """will create a fresh img (with menu) and send it to the displayer
-        :returns: TODO
 
         """
-        pass
+        self._reset_img()
+        img_for_displayer = self._img.copy()
+        self._displayer.display_img(img_for_displayer, sleep_after=False)
 
     def _reset_img(self):
-        pass
+        self._img = Image.new()
 
     def _process_coordinates_loop(self):
         while True:
