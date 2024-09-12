a561f400b8f2f3972f87020159fa33a4760559a6
Imam Usmani <imam@e.email>
2024-09-12 21:46:06 +0200

    use context on drawtool to terminates thread non daemonic

--- src/epaper_telegram/models/img_creators.py
+++ src/epaper_telegram/models/img_creators.py
@@ -1,9 +1,13 @@
 import time
-from threading import Thread
+from threading import Thread, Event
 from queue import Queue
+import logging
 
 
 from PIL import Image, ImageDraw
+
+
+class DrawToolError(Exception): pass
 
 
 class DrawTool(object):
@@ -13,20 +17,16 @@ class DrawTool(object):
     or not"""
 
     def __init__(self, displayer):
-        """TODO: to be defined.
+        """
         :displayer: Displayer objet that uses the epd
         """
         self._displayer = displayer
         self._img = None
 
         self._queue = Queue()
-        self._thread = Thread(
-                target=self._process_coordinates_loop,
-                daemon=True,
-                )
-        self._thread.start()
-
-        self._reset_img()
+        self._thread = Thread(target=self._process_coordinates_loop)
+        self._running = Event()
+        self._running.set()
 
     def point_to(self, x, y):
         """
@@ -42,6 +42,7 @@ class DrawTool(object):
         :returns: continue (boolean) and img (None or array)
 
         """
+        self._check_started()
         pass
 
     def clear_img(self):
@@ -52,6 +53,41 @@ class DrawTool(object):
         img_for_displayer = self._img.copy()
         self._displayer.display_img(img_for_displayer, sleep_after=False)
 
+    def _check_started(self):
+        if self._thread.is_alive() is not True:
+            msg = (
+                    'thread has not started or has been terminated.',
+                    'use start method or context manager',
+                    )
+            logging.exception(msg)
+            raise DrawToolError(msg)
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, ex_type, ex_value, ex_traceback):
+        if self._thread.is_alive():
+            self.terminate()
+
+    def start(self):
+        """start thread that will take handles coordinates input to display
+
+        """
+        if self._thread.is_alive():
+            msg = 'thread has already started'
+            logging.exception(msg)
+            raise DisplayerError(msg)
+        self.clear_img()
+        self._thread.start()
+
+    def terminate(self):
+        """terminate the thread
+
+        """
+        self._check_started()
+        self._running.clear()
+
     def _reset_img(self):
         img = Image.new('1', (250, 122), 255)
         draw = ImageDraw.Draw(img)
@@ -59,5 +95,7 @@ class DrawTool(object):
         self._img = img
 
     def _process_coordinates_loop(self):
-        while True:
+        while self._running.is_set():
+            self._displayer.wait_for_ready()
             self._queue.get()
+        logging.info('terminates drawtool thread')
--- tests/test_img_creators.py
+++ tests/test_img_creators.py
@@ -1,12 +1,15 @@
+import logging
+
+
 from epaper_telegram.models.img_creators import DrawTool
 from epaper_telegram.models.display import Displayer
 
 
 def draw_tool():
+    logging.basicConfig(level=logging.INFO)
     with Displayer(mock_mode=True) as displayer:
-        drawtool = DrawTool(displayer)
-        drawtool.clear_img()
-        input()
+        with DrawTool(displayer) as draw_tool:
+            input()
 
 
 if __name__ == "__main__":
