c6cf51d6606b612e1d94442d6352dfb117127d26
Imam Usmani <imam@e.email>
2024-09-14 22:15:39 +0200

    add test clear img

--- tests/test_img_creators.py
+++ tests/test_img_creators.py
@@ -1,4 +1,5 @@
 import logging
+import unittest
 import time
 
 
@@ -6,13 +7,37 @@ from epaper_telegram.models.img_creators import DrawTool
 from epaper_telegram.models.display import Displayer
 
 
+class TestMyClass(unittest.TestCase):
+
+    """all test concerning my class. """
+
+    @classmethod
+    def setUpClass(cls):
+        cls.displayer = Displayer(mock_mode=True)
+        cls.displayer.start()
+        cls.draw_tool = DrawTool(cls.displayer)
+        cls.draw_tool.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.draw_tool.terminate()
+        cls.displayer.terminate()
+
+    # def test_point_to(self):
+        # pass
+
+    def test_clear_img(self):
+        self.draw_tool.clear_img()
+
+
 def draw_tool():
     logging.basicConfig(level=logging.INFO)
     with Displayer(mock_mode=True) as displayer:
         with DrawTool(displayer) as draw_tool:
-            time.sleep(2)
-            draw_tool.point_to(125, 61, 9)
-            time.sleep(2)
+            draw_tool.clear_img()
+            # time.sleep(2)
+            # draw_tool.point_to(125, 61, 9)
+            # time.sleep(2)
 
 
 if __name__ == "__main__":
