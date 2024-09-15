1ff1a8e7d7828ddacc617fdc5a4fa2e5af6b2971
Imam Usmani <imam@e.email>
2024-09-15 22:12:32 +0200

    include draw tool and displayer in app

--- src/epaper_telegram/apps.py
+++ src/epaper_telegram/apps.py
@@ -5,6 +5,8 @@ from threading import Thread
 
 from waveshare_touch_epaper.touch_screen import GT1151
 from epaper_telegram.models.mocks import GT1151Mock
+from epaper_telegram.models.img_creators import DrawTool
+from epaper_telegram.models.display import Displayer
 
 
 class EpaperTelgramApp(object):
@@ -16,6 +18,7 @@ class EpaperTelgramApp(object):
             self._GT = GT1151Mock
         else:
             self._GT = GT1151
+        self._mock_mode = mock_mode
 
     def start(self):
         """method to start the app.
@@ -28,12 +31,22 @@ class EpaperTelgramApp(object):
 
 
         try:
-            with self._GT() as gt:
+            with (
+                    self._GT() as gt,
+                    Displayer(mock_mode=self._mock_mode) as displayer,
+                    ):
+                logging.info('TODO: start thread download img and display')
                 while True:
                     logging.info('home')
                     gt.wait_for_gesture()
                     logging.info('open draw mode...')
-                    gt.input()
+                    with DrawTool(displayer) as draw_tool:
+                        to_continue = True
+                        while to_continue:
+                            coordinates = gt.input()
+                            to_continue, img = draw_tool.point_to(*coordinates)
+                        if img is not None:
+                            logging.info('TODO: upload image')
         except KeyboardInterrupt:
             logging.info('app stopped by keyboard interrupt')
 
