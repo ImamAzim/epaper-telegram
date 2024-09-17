ddb9e1ae9b82831ad11daec1c88ec20575cea97a
Imam Usmani <imam@e.email>
2024-09-17 21:46:27 +0200

    test download

---
+++ tests/test_online_tools.py
@@ -0,0 +1,44 @@
+
+"""
+test online tool module
+"""
+
+import unittest
+from threading import RLock
+import time
+import logging
+
+
+from PIL import Image, ImageDraw
+
+
+from epaper_telegram.models.online_tools import OnlineImg
+
+
+class TestMyClass(unittest.TestCase):
+
+    """all test concerning OnlineImg """
+
+    @classmethod
+    def setUpClass(cls):
+        cls.online_img_tool = OnlineImg(mock_mode=True)
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_download(self):
+        img = self.online_img_tool.download()
+        self.assertIsInstance(img, Image.Image)
+
+""" script tests """
+
+
+def download():
+    logging.basicconfig(level=logging.info)
+    online_img_tool = OnlineImg()
+    img.show()
+
+
+if __name__ == '__main__':
+    download()
