6f34a1bdf534000623902db7167380655287cab5
Imam Usmani <imam@e.email>
2024-09-22 21:53:29 +0200

    add img convertor

---
+++ src/epaper_telegram/models/tools.py
@@ -0,0 +1,24 @@
+class ImgConvertor(object):
+
+    """class to convert img to string and vice versa, so that it can transit in a jabber message"""
+
+    def __init__(self):
+        """TODO: to be defined. """
+
+    def img2str(self, img):
+        """convert img to a string
+
+        :img: PIL Image
+        :returns: str
+
+        """
+        pass
+
+    def str2img(self, img_str):
+        """convert a string to a PIL Image
+
+        :img_str: str
+        :returns: PIL Image
+
+        """
+        pass
