614a873f8cae8657091a5e135fa72c2428d0d09b
Imam Usmani <imam@e.email>
2024-09-23 20:59:39 +0200

    test send

---
+++ tests/test_xmpp.py
@@ -0,0 +1,24 @@
+from PIL import Image, ImageDraw
+
+
+from epaper_telegram.models.xmpp import CredentialsHandler, CredentialsHandlerError
+from epaper_telegram.models.xmpp import ReceiverBot, SenderBot
+
+
+def send():
+    corresp_jid = input('correspondant: ')
+
+    credential_handler = CredentialsHandler()
+    try:
+        credentials = credential_handler.load_credentials()
+    except FileNotFoundError:
+        credential_handler.create_and_save_new_cred()
+        credentials = credential_handler.load_credentials()
+
+    sender = SenderBot(corresp_jid=corresp_jid, **credentials)
+
+    img = Image.new('1', (200, 100), 255)
+    draw = ImageDraw.Draw(img)
+    draw.text(((100, 50), 'salut!'))
+    sender.send_img(img)
+
