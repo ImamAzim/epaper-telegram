25c510d703f2169edf18d0abc284c96597553112
Imam Usmani <imam@e.email>
2024-09-25 20:31:54 +0200

    fix arg name corresp

--- src/epaper_telegram/apps.py
+++ src/epaper_telegram/apps.py
@@ -44,7 +44,7 @@ class EpaperTelgramApp(object):
                 with OnlineImageDownloader(
                         displayer,
                         self._credentials,
-                        correspondant=self._corresp_jid,
+                        corresp_jid=self._corresp_jid,
                         ) as online_image_downloader:
                     while True:
                         logging.info('home')
