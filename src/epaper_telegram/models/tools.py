e20a7829300b233d257f38c7f286a3f6f79b879b
Imam Usmani <imam@e.email>
2024-09-27 23:38:19 +0200

    use sys.exec_prefix

--- src/epaper_telegram/models/tools.py
+++ src/epaper_telegram/models/tools.py
@@ -21,11 +21,7 @@ class Configurator(object):
 
         """
 
-        executed_path = sys.argv[0]
-        abso_executed_path = os.path.abspath(executed_path)
-        bin_dir = os.path.dirname(abso_executed_path)
-        exec_path = os.path.join(bin_dir, DAEMON_ENTRY_POINT)
-        print(exec_path)
+        exec_path = os.path.join(sys.exec_prefix, 'bin', DAEMON_ENTRY_POINT)
         if not os.path.exists(exec_path):
             msg = 'there is no script to run the daemon app'
             raise ConfiguratorError(msg)
