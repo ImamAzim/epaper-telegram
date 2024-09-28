3156796b9d97fe4af29fa225c341c44440f0f0e1
Imam Usmani <imam@e.email>
2024-09-28 20:42:21 +0200

    create cron job

--- src/epaper_telegram/models/tools.py
+++ src/epaper_telegram/models/tools.py
@@ -1,5 +1,9 @@
+import getpass
 import os
 import sys
+
+
+from crontab import CronTab
 
 
 from epaper_telegram import DAEMON_ENTRY_POINT
@@ -20,13 +24,24 @@ class Configurator(object):
         """to enable the app
 
         """
+        comment = 'epaper-telegram'
 
         exec_path = os.path.join(sys.exec_prefix, 'bin', DAEMON_ENTRY_POINT)
         if not os.path.exists(exec_path):
             msg = 'there is no script to run the daemon app'
+            print(msg)
             raise ConfiguratorError(msg)
-        else:
-            print('TODO: add crontab job')
+
+        cron = CronTab(user=getpass.getuser())
+        if [el for el in cron.find_comment(comment)]:
+            msg = 'epaper-telegram is already activated'
+            print(msg)
+            raise ConfiguratorError
+
+        tmp_file = f'/tmp/epaper-telegram_log_{getpass.getuser()}'
+        job = cron.new(command=f'{exec_path} -l > {tmp_file} 2>&1', comment=comment)
+
+        cron.write()
 
 
     def remove_daemon_from_crontab(self):
